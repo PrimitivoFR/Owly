7f4b73b8ece07653ebb60505a489a76e04b711ab
AppliNH <thomas.martindev@gmail.com>
2020-09-09 17:27:52 +0200

    setting up everything before setting up envy

--- owly-client/src/app/app.component.ts
+++ owly-client/src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { GRPC_CLIENT_FACTORY } from '@ngx-grpc/core';
+import { GrpcWebClientBase } from 'grpc-web';
+import { GrpcClientSettings, GrpcStatusEvent } from '@ngx-grpc/common';
+import { GRPC_USER_SERVICE_CLIENT_SETTINGS } from 'src/proto/user.pbconf';
+import { CreateNewUserRequest, CreateNewUserResponse } from 'src/proto/user.pb';
+import { UserServiceClient } from 'src/proto/user.pbsc';
+
+
+interface CreateNewUserEvent {
+  request?: CreateNewUserRequest,
+  response?: CreateNewUserResponse,
+  status?: GrpcStatusEvent,
+  error?: GrpcStatusEvent
+}
 
 @Component({
   selector: 'app-root',
+  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'owly-client';
+  constructor(private userClient: UserServiceClient) {}
+
+  private createUserRequest() {
+    return new CreateNewUserRequest(
+      { firstName: "Séraphin",
+        lastName: "Henry",
+        username:"the_capi",
+        email: "seraphin.henry@primitivo.fr",
+        password:"bruh" 
+      })
+  }
+
+  ngOnInit(): void {
+
+    const request = this.createUserRequest();
+
+    this.userClient.createNewUser(request).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );
+    
+  }
 }
--- owly-client/src/app/app.module.ts
+++ owly-client/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GRPC_CLIENT_FACTORY } from '@ngx-grpc/core';
+import { GrpcWebClientFactory } from '@ngx-grpc/grpc-web-client';
+import {GRPC_USER_SERVICE_CLIENT_SETTINGS} from '../proto/user.pbconf'
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: GRPC_CLIENT_FACTORY, useClass: GrpcWebClientFactory },
+    { provide: GRPC_USER_SERVICE_CLIENT_SETTINGS, useValue: { host: 'http://0.0.0.0:50051' } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
