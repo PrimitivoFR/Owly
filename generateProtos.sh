a1784d45d85870ad991366277a4259eb21a2ef06
AppliNH <thomas.martindev@gmail.com>
2020-09-19 23:39:30 +0300

    adding scripts to generate message service

--- generateProtos.sh
+++ generateProtos.sh
@@ -3,3 +3,4 @@
 # Golang
 protoc protofiles/user.proto --go_out=plugins=grpc:./server/
 protoc protofiles/chatroom.proto --go_out=plugins=grpc:./server/
+protoc protofiles/message.proto --go_out=plugins=grpc:./server/
