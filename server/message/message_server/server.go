7307c9b563b9ebad38746486613f8fa0b2177869
Adil Mohammad <adil.mohammad@primitivo.fr>
2020-09-20 16:49:05 +0300

    OWL-28-from-OWL-9 : Send Message

--- server/message/message_server/server.go
+++ server/message/message_server/server.go
@@ -1,6 +1,8 @@
 package message_server
 
 import (
+	"strings"
+	"context"
 	"encoding/json"
 	"log"
 	"net"
@@ -11,6 +13,8 @@ import (
 	"github.com/rgamba/evtwebsocket"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
 )
 
 type server struct{}
@@ -70,7 +74,57 @@ func (*server) StreamMessagesByChatroom(req *messagepb.StreamMessagesByChatroomR
 
 	return wsError
 }
+func (*server) SendMessage(ctx context.Context, req *messagepb.SendMessageRequest) (*messagepb.SendMessageResponse, error) {
 
+	// Create a mapping for the Elasticsearch documents
+	var docMap map[string]interface{}
+	
+
+	// Declare an Elasticsearch configuration
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"http://elasticsearch:9400",
+		},
+	}
+
+	// Connect to ElasticSearch
+	client, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		log.Printf("Elasticsearch connection error %v", err)
+	}
+
+	doc, err := json.Marshal(req.Message)
+	if err != nil {
+		log.Printf("Json Marshaling returned error : %v", err)
+	}
+
+	indexRequest := esapi.IndexRequest{
+		Index : req.Message.ChatroomID,
+		Body : strings.NewReader(string(doc)),
+		Refresh : "true",
+	}
+
+	res, err := indexRequest.Do(ctx, client)
+	if err != nil {
+		log.Printf("Inserting index failed: %v", err)
+	}
+	defer res.Body.Close()
+
+	
+	if res.IsError() {
+		log.Printf("%s error index document ID=%d", res.Status())
+	}
+	if err := json.NewDecoder(res.Body).Decode(&docMap); err != nil {
+		log.Printf("Error parsing the response body: %s", err) 
+	}
+	log.Println("resMap", docMap)
+	
+	response := messagepb.SendMessageResponse{
+		Success: true,
+	}
+	return &response, nil
+
+}
 //
 func StartServer() {
 
