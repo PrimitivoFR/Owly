0c152ce91130103106c3a34a4474813c16a9d6d0
S-HENR <seraphin.henry@orange.fr>
2020-09-21 12:23:38 +0300

    Add a logout button to the home page

--- owly-client/src/app/home/home.component.ts
+++ owly-client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/_services/auth.service';
 import { SnackAlertService } from '../common/components/snack-alert/snack-alert.service';
 
 @Component({
@@ -12,12 +13,17 @@ export class HomeComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private snackService: SnackAlertService,
+    private authService: AuthService,
   ) { }
 
   ngOnInit(): void {
     if (this.route.snapshot.queryParams['login']) {
       this.snackService.showSnack('Connection successfully');
     }
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 }
