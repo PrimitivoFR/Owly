83e5bf8e50fb4b61bbf46a1db217c133bc9fe6fc
AppliNH <thomas.martindev@gmail.com>
2020-09-29 00:06:42 +0300

    skipping the interceptor intel in case of reflection: - Without this, we can't even open Evans CLI for the targeted gRPC serv. - Seems like the streamInterceptor blocks the bi-di stream with Evans. - Can't make it work either with --header flag

--- server/common/interceptors/stream.go
+++ server/common/interceptors/stream.go
@@ -2,6 +2,7 @@ package interceptors
 
 import (
 	"log"
+	"strings"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -11,6 +12,13 @@ import (
 
 func StreamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 	md, ok := metadata.FromIncomingContext(ss.Context())
+
+	if strings.Contains(info.FullMethod, "reflection") {
+		// Without this, we can't use reflection (with Evans), because it gets blocked by the stream interceptor
+		// which asks for a token. So we can't even open a CLI.
+		// This even occurs when using --header flag
+		return handler(srv, ss)
+	}
 
 	if !ok {
 		return status.Errorf(codes.Internal, "Invalid Token or Internal error")
