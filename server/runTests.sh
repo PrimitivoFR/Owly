5639f514b793305a162c8893d673232a4fb717c4
vgalin <47885533+vgalin@users.noreply.github.com>
2020-10-05 10:48:56 +0200

    fix bad file formatting and add warning

--- server/runTests.sh
+++ server/runTests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 
+# when editing this file on windows, be sure to set the end of line sequence to LF,
+# or it will cause an error in the likes of :
+# bash: ./runTests.sh: /bin/bash^M: bad interpreter: No such file or directory
+
 go test ./auth/auth_server
 go test ./user/user_server
