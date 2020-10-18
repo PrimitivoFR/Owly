9a61599c752ac69ac62a805dea26b70bb5dd19da
AppliNH <thomas.martindev@gmail.com>
2020-10-18 22:39:42 +0300

    OWL-59: + Find one method in ORMMongo interface + Implemented the new method everywhere

--- server/common/mongo/repository.go
+++ server/common/mongo/repository.go
@@ -3,11 +3,13 @@ package common_mongo
 import (
 	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type MongoORM interface {
 	ORMInsertOne(document interface{}) (*mongo.InsertOneResult, error)
+	ORMFindOneById(id interface{}) (*mongo.SingleResult, error)
 	CheckConnectivity() error
 }
 
@@ -42,4 +44,24 @@ func (collection *MongoEntity) CheckConnectivity() error {
 		}
 	}
 	return nil
+}
+
+func (collection *MongoEntity) ORMFindOneById(id interface{}) (*mongo.SingleResult, error) {
+	// id could be an ObjectID or a simple string ID. No conversion is done here
+
+	if err := collection.CheckConnectivity(); err != nil {
+		return nil, err
+	}
+
+	collec := collection.c
+	entity := *collec
+
+	// find document by _id
+	result := entity.FindOne(
+		context.Background(),
+		bson.M{"_id": id},
+	)
+
+	return result, nil
+
 }
