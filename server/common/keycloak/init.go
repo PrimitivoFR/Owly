08b506f1a1fa3b7dce554d5dcaaade25d6cea0bf
AppliNH <thomas.martindev@gmail.com>
2021-01-21 14:41:13 +0100

    fixing testing

--- server/common/keycloak/init.go
+++ server/common/keycloak/init.go
@@ -12,16 +12,19 @@ import (
 
 var keycloak_admin_user string
 var keycloak_admin_pass string
+var keycloak_hostname string
 
 func loadEnvVars() {
+	log.Println("Loading env vars")
 	keycloak_admin_user = os.Getenv("KEYCLOAK_USER")
 	keycloak_admin_pass = os.Getenv("KEYCLOAK_PASSWORD")
+	keycloak_hostname = os.Getenv("KEYCLOAK_HOSTNAME")
 
 }
 
 func checkConnection() {
 	for {
-		res, err := http.Get("http://keycloak-srv:8080/auth/")
+		res, err := http.Get("http://" + keycloak_hostname + ":8080/auth/")
 		if err == nil && res != nil {
 			if res.StatusCode == 200 {
 				log.Println("Keycloak instance up, ready and reachable !")
@@ -36,14 +39,14 @@ func checkConnection() {
 }
 
 func InitAdmin() (*AdminGuy, error) {
-
+	loadEnvVars()
 	checkConnection()
-	client := gocloak.NewClient("http://keycloak-srv:8080/")
+	client := gocloak.NewClient("http://" + keycloak_hostname + ":8080/")
 	if client == nil {
 		log.Fatal()
 		return nil, errors.Errorf("Couldn't connect to Keycloak instance")
 	}
-	loadEnvVars()
+
 	adminGuy, err := NewAdmin(keycloak_admin_user, keycloak_admin_pass, client)
 
 	return adminGuy, err
